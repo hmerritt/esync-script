23a46aee24d97105d43bb7fb9966e40dc066e485
Harry Merritt <harry@merritt.es>
2021-01-07 23:33:33 +0000

    print script version and install script dependencies
    esync version
    esync install

--- src/esync.sh
+++ src/esync.sh
@@ -7,3 +7,40 @@ ARGS=("$@")
 ## Import modules
 source "modules/module-loader.sh"
 loadmodules "${modules}" "modules"
+
+
+##------------------------------------------------------------------------------
+
+
+## Check if
+if [ "${#ARGS[1]}" == "0" ]; then
+
+
+	## Print script version
+	if [ "${ARGS[0]}" == "version" ]; then
+		green "esync [Version ${VERSION}]\n"
+		exit
+	fi
+
+
+	## Install script dependencies
+	if [ "${ARGS[0]}" == "install" ]; then
+		task "Install script dependencies"
+
+		actionsub "Update package repo"
+		ERROR=$(apt-get update -y 2>&1)
+		onfail "" "Are you root?"
+		result "ok"
+
+		actionsub "Install rsync"
+		ERROR=$(apt-get install rsync -y 2>&1)
+		onfail "" "${ERROR}"
+		result "ok"
+
+		echo
+		success "Install complete"
+		exit
+	fi
+
+
+fi
