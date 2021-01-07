5714e889a0116821ee12455c5a531162c9abd310
Harry Merritt <harry@merritt.es>
2021-01-07 23:34:36 +0000

    add experimental script update argument
    - checks for updates on github and then fetches a new version

--- src/esync.sh
+++ src/esync.sh
@@ -43,4 +43,65 @@ if [ "${#ARGS[1]}" == "0" ]; then
 	fi
 
 
+	## Update script to latest version
+	## (overwrites itself)
+	if [ "${ARGS[0]}" == "update" ]; then
+		task "Update script to latest version"
+
+		actionsub "Checking for newer version"
+		github_tags_url="https://api.github.com/repos/hmerritt/esync-script/tags"
+		version_latest=$(curl -s "${github_tags_url}" | grep -Po -m 1 '[^v]*[0-9]\.[0-9]\.[0-9]')
+		onfail "" "${version_latest}"
+		result "ok"
+
+		## Compare current version with latest
+		## Prevents needless update
+		if [ "${VERSION//.}" -ge "${version_latest//.}" ]; then
+			echo
+			green "No new update available"
+			green "-> v${version_latest} is latest"
+			echo
+			exit 0
+		fi
+
+		actionsub "Fetching latest version"
+		github_file="https://github.com/hmerritt/esync-script/releases/download/v${version_latest}/esync.sh"
+		cd "/var/tmp" || "/tmp"
+		ERROR=$(curl -L "${github_file}" -o "esync.sh" 2>&1)
+		onfail "" "${ERROR}"
+
+		## Varify downloaded file
+		first_line=$(head -n 1 "esync.sh")
+		if [ "${first_line}" != "#!/bin/bash" ]; then
+			echo
+			error "Failed to fetch latest version: v${version_latest}"
+			error "-> Could not verify downloaded file"
+			echo
+			warning "You could try fetching it manually from GitHub"
+			warning "${github_file}"
+			echo
+			failure "Update failed"
+
+			rm "esync.sh"
+			exit 1
+		fi
+		onfail
+		result "ok"
+
+		actionsub "Replacing script with newer version"
+		ERROR=$(mv "esync.sh" "${SCRIPT_PATH}/${SCRIPT_NAME}" 2>&1)
+		onfail "" "${ERROR}"
+		ERROR=$(chmod +x "${SCRIPT_PATH}/${SCRIPT_NAME}" 2>&1)
+		onfail "" "${ERROR}"
+		result "ok"
+
+		echo
+		green "Updated: ${VERSION} --> ${version_latest}"
+
+		echo
+		success "Update complete"
+		exit
+	fi
+
+
 fi
